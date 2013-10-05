01f1faa2424921aa9dea0a10d29ea9d108cdc579
balaga <kurniawan.dn@gmail.com>
2013-10-06 00:05:44 +0700

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_Primo_H2.mk
---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := Primo_H2
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Walton/Primo_H2/device_Primo_H2.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Primo_H2
+PRODUCT_NAME := cm_Primo_H2
+PRODUCT_BRAND := Walton
+PRODUCT_MODEL := Primo_H2
+PRODUCT_MANUFACTURER := Walton
